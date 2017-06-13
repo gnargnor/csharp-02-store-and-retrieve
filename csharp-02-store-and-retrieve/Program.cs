dcfa26f5ef7880a3f3a35174330b426da7e1f0ae
Logan Kelly <logan.r.kelly@gmail.com>
2017-06-13 17:30:35 -0500

    how many fellas?

---
+++ csharp-02-store-and-retrieve/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace csharp02storeandretrieve
+{
+    class MainClass
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hey! What do you call yer band?");
+            string name = Console.ReadLine();
+
+            Console.WriteLine("How many fellas does yer band be made up of?");
+            int numberOfMembers = int.Parse(Console.ReadLine());
+
+            Console.WriteLine(name + " has " + numberOfMembers + " members.");
+        }
+    }
+}
